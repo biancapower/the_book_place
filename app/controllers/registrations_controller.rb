a2af360337ee321b84dcfee12449e182f2852317
Bianca Power <drbiancampower@gmail.com>
2017-11-02 11:52:31 +1100

    redirects to create new profile after signup

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < Devise::RegistrationsController
+  private
+
+  # Modified Devise params for user login
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def after_sign_up_path_for(resource)
+    "/profiles/new"
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    "/profiles/new"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   resources :profiles
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
 
