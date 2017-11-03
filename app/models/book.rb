8ab3277b5ce5205b548a2cbcf9176ff01c2e167a
Bianca Power <drbiancampower@gmail.com>
2017-11-03 12:00:36 +1100

    can search by title, author, category, isbn10, or isbn13

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
 
   def self.search(search)
-    where("title ILIKE ?", "%#{search}%") 
+    where("title ILIKE ? OR author ILIKE ? OR category ILIKE ? OR isbn10 ILIKE ? OR isbn13 ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
   end
 
 end
