9688bb75386c9ee287a5556edaa2a559639d2e06
Bianca Power <drbiancampower@gmail.com>
2017-11-05 19:58:53 +1100

    added some rspec tests to validate user and profile

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with username, first name, last name, street address, suburb, postcode, and bio" do
+    profile = Profile.new(
+      user_id: "1",
+      username: "read_lots",
+      first_name: "Joe",
+      last_name: "Bloggs",
+      street_address: "123 Main St",
+      suburb: "Blacktown",
+      postcode: "0129",
+      bio: "I love to read books :)"
+      )
+    expect(profile).to be_valid
+  end
+
+  it "is invalid without a username" do
+    profile = Profile.new(username: nil)
+    profile.valid?
+    expect(profile.errors[:username]).to include("can't be blank")
+  end
+
+
+  it "is invalid with a duplicate username" do
+    Profile.create(
+      first_name:  "Bill",
+      last_name:  "Bloggs",
+      username:   "book_fan"
+    )
+    profile = Profile.new(
+      first_name:  "Bill",
+      last_name:  "Roe",
+      username:   "book_fan"
+    )
+    profile.valid?
+    expect(profile.errors[:username]).to_not include("has already been taken")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with an email address and password" do
+    user = User.new(
+      email: "bob@gmail.com",
+      password: "ah9ud_ihau"
+      )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid without a password" do
+    user = User.new(password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+      email:      "bill@example.com",
+      password:   "yugugiuiuih",
+    )
+    user = User.new(
+      email:      "bill@example.com",
+      password:   "sjnkoigy",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
 end
